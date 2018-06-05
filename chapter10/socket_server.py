9217bff6f7cf854320ce86e9d6fe902a274c04fe
吴富林 <wfl@eie.ltd>
2018-06-05 17:46:03 +0800

    socket编程

---
+++ chapter10/socket_server.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+__author__ = 'wufulin'
+
+import socket
+import threading
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 8000))
+server.listen()
+
+def handle_sock(sock, addr):
+    while True:
+        data = sock.recv(1024)
+        print(data.decode('utf8'))
+        re_data = input()
+        sock.send(re_data.encode('utf8'))
+    sock.close()
+
+
+# 获取从客户端发送的数据
+# 一次获取1k的数据
+while True:
+    sock, addr = server.accept()
+
+    # 用线程去处理新接收的连接
+    client_thread = threading.Thread(target=handle_sock, args=(sock, addr))
+    client_thread.start()
+
+    # sock.send("hello {}".format(data.decode('utf8')).encode('utf8'))
+    # server.close()
+    # sock.close()
