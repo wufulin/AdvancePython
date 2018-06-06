ad0be2c0fbcb7d44908bbdc728ecefe17386a598
吴富林 <wfl@eie.ltd>
2018-06-06 17:07:53 +0800

    mro查找

---
+++ chapter04/attr_mro.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+__author__ ='wufulin'
+
+# 类属性和实例属性的查找顺序
+class A:
+    name = 'A'
+    # def __init__(self):
+    #     self.name = 'obj'
+
+# C3算法
+a = A()
+print(a.name)
+
+# 新式类
+class D:
+    pass
+
+class C(D):
+    pass
+
+class B(D):
+    pass
+
+class M(B, C):
+    pass
+
+print(M.__mro__)
