8f4910fea92461e425148c630967898415943c65
吴富林 <wfl@eie.ltd>
2018-06-07 17:07:41 +0800

    array数组

---
+++ chapter05/array_test.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+__author__ ='wufulin'
+
+# array, deque
+# 数组
+import array
+# array和list的一个重要区别， array只能存放指定的数据类型
+my_array = array.array("i")
+my_array.append(1)
+my_array.append(2)
+print(my_array)
