938150922fbfa9272794e9b3f8912b5a6e7a35f4
吴富林 <wfl@eie.ltd>
2018-06-07 14:15:22 +0800

    with上下文管理器

---
+++ chapter04/with_test.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+__author__ ='wufulin'
+
+# try except finally
+def exe_try():
+    try:
+        # f_read = open("bobby.txt")
+        print("code started")
+        # f_read.close()
+        raise KeyError
+    except KeyError as e:
+        print("key error")
+        # f_read.close()
+        return 2
+    else:
+        print("other error")
+        return 3
+    finally:
+        print("finally")
+        # f_read.close()
+        return 4
+
+# 上下文管理器协议
+class Sample:
+    def __enter__(self):
+        print("enter")
+        # 获取资源
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # 释放资源
+        print("exit")
+        
+    def do_something(self):
+        print("doing something")
+
+with Sample() as sample:
+    sample.do_something()
+
+if __name__ == "__main__":
+    result = exe_try()
+    # print(result)
